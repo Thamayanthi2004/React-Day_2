e2b7ebdae172300f81c33add92f23833dc2670fa
Thamayanthi2004 <146456968+Thamayanthi2004@users.noreply.github.com>
2024-08-17 06:32:44 +0530

    Create Welcome.js

---
+++ Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomeClass from './Components';
+
+function App() {
+  return (
+    <div>
+      <WelcomeClass name="John" />
+      <WelcomeClass name="Jane" />
+    </div>
+  );
+}
+
+export default App;
